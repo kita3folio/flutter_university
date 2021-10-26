2ef42796f59382767599ffd85f567bfc1ab483fd
kita3folio <mk20181216@gmail.com>
2021-10-27 08:55:30 +0900

    providerという形でリファクタリングする

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_university/book_list_page.dart';
+import 'package:flutter_university/book_list/book_list_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
