81e79276241acb1eb56f3c30878626260ee888cd
kita3folio <mk20181216@gmail.com>
2021-10-31 16:47:17 +0900

    image_pickerを使って画像を追加する処理の実装

--- lib/add_book/add_book_page.dart
+++ lib/add_book/add_book_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_university/add_book/add_book_model.dart';
-import 'package:flutter_university/domain/book.dart';
 import 'package:provider/provider.dart';
 
 class AddBookPage extends StatelessWidget {
@@ -15,48 +14,76 @@ class AddBookPage extends StatelessWidget {
         ),
         body: Center(
           child: Consumer<AddBookModel>(builder: (contex, model, child) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: '本のタイトル',
+            return Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                          child: SizedBox(
+                            width: 100,
+                            height: 160,
+                            child: model.imageFile != null
+                                ? Image.file(model.imageFile!)
+                                : Container(
+                                    color: Colors.grey,
+                                  ),
+                          ),
+                          onTap: () async {
+                            await model.pickImage();
+                          }),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: '本のタイトル',
+                        ),
+                        onChanged: (text) {
+                          model.title = text;
+                        },
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: '本の著者',
+                        ),
+                        onChanged: (text) {
+                          model.author = text;
+                        },
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      ElevatedButton(
+                          onPressed: () async {
+                            try {
+                              model.startLoading();
+                              await model.addBook();
+                              Navigator.of(context).pop(true);
+                            } catch (e) {
+                              final snackBar = SnackBar(
+                                backgroundColor: Colors.red,
+                                content: Text(e.toString()),
+                              );
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                            } finally {
+                              model.endLoading();
+                            }
+                          },
+                          child: Text('追加する'))
+                    ],
+                  ),
+                ),
+                if (model.isLoading)
+                  Container(
+                    color: Colors.black54,
+                    child: Center(
+                      child: CircularProgressIndicator(),
                     ),
-                    onChanged: (text) {
-                      model.title = text;
-                    },
                   ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: '本の著者',
-                    ),
-                    onChanged: (text) {
-                      model.author = text;
-                    },
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  ElevatedButton(
-                      onPressed: () async {
-                        try {
-                          await model.addBook();
-                          Navigator.of(context).pop(true);
-                        } catch (e) {
-                          final snackBar = SnackBar(
-                            backgroundColor: Colors.red,
-                            content: Text(e.toString()),
-                          );
-                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                        }
-                      },
-                      child: Text('追加する'))
-                ],
-              ),
+              ],
             );
           }),
         ),
